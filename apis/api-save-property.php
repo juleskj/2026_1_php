22c3a3ea8cc0b7504ecffd3a12d0f6d0f694d7bf
Jules KJ <juljen2730@gmail.com>
2026-04-09 21:50:30 +0200

    function for saving property when user is logged in

---
+++ apis/api-save-property.php
@@ -0,0 +1,14 @@
+<?php
+
+$item_pk = $_GET["item_pk"] ?? "";
+
+//TODO: validate item pk
+// TODO: make sure user is in session
+// TODO: get user id
+// TODO: connect db
+// TODO: inset into db
+
+// TODO:return full heart icon to replace heart icon
+
+echo "ok";
+echo $item_pk;
