d872250daef5b187ac6e095edab093b726a1df60
Jules KJ <juljen2730@gmail.com>
2026-04-13 22:16:28 +0200

    reverification email sent and succsesfull

--- apis/api-resend-verification.php
+++ apis/api-resend-verification.php
@@ -16,8 +16,23 @@
 
     $user = $stmt->fetch();
 
-
     echo json_encode($user);
     
 
-    echo $token;
+    date_default_timezone_set('Europe/Copenhagen'); //makes sure its the correct time stamp
+
+    $new_token = bin2hex(random_bytes(25)); // Returns 50 characters
+    $new_expire_date = date('Y-m-d H:i:s', strtotime('+1 hour'));
+
+    $sql = "UPDATE users SET verification_token= :new_token, token_expires_at= :new_expire_date 
+            WHERE user_email = :user_email";
+
+    $stmt = $_db->prepare($sql);
+    $stmt->bindValue(":new_token", $new_token);
+    $stmt->bindValue(":new_expire_date", $new_expire_date);
+    $stmt->bindValue(":user_email", $user["user_email"]);
+    $stmt->execute();
+    
+    _send_welcome_email($user["user_email"],$new_token, true);
+
+    echo"ok";
