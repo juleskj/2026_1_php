3f5b042f259679467979c5faa88b32a700747af8
Jules KJ <juljen2730@gmail.com>
2026-04-13 16:11:55 +0200

    installed composer php packaged manager to use phpmailer so i can send verification mail
    email sending succsefully working

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,26 @@
 FROM php:8.2-apache
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    git \
+    unzip \
+    && docker-php-ext-install pdo pdo_mysql
+
+# Install Composer
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+# Install Redis extension
 RUN pecl install redis && docker-php-ext-enable redis
-RUN docker-php-ext-install pdo pdo_mysql
-RUN a2enmod rewrite 
-# a2enmod for friendly url
+
+# Enable Apache rewrite module
+RUN a2enmod rewrite
+
+# Copy composer files
+WORKDIR /var/www/html
+COPY composer.json composer.lock ./
+
+# Install PHP dependencies
+RUN composer install --no-dev --optimize-autoloader
+
+# Copy the rest of the application
+COPY . .
--- apis/api-login.php
+++ apis/api-login.php
@@ -1,9 +1,24 @@
 <?php
 
-try{
-    session_start();
+session_start();
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__."/../_.php";
 
-    require_once __DIR__."/../_.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+
+
+try{   
+    
+    $mail = new PHPMailer(true);
+
+    $sender_email = "juicefrog2610@gmail.com";
+    $email_password = "uwms bmfe bnhk qajz" ; 
+
+
 
     $user_email = _validate_user_email();
     $user_password = _validate_user_password();
@@ -41,6 +56,40 @@ try{
             'user_forename' => $user['user_forename'],
         ];
 
+
+    // TODO:make into funktion 
+    // sending a verification email
+    // TODO: welcome mail when verifired
+    // TODO: welcome mail when verifired
+    // TODO:reset password
+
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = $sender_email;
+    $mail->Password   = $email_password;
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+
+    $mail->setFrom($sender_email, 'JuiceFrog');
+    $mail->addAddress($user_email);
+
+    $mail->isHTML(true);
+
+    $mail->Subject = 'Welcome to boligsiden';
+
+    $mail->Body = "
+        <h2>Welcome!</h2>
+        <p>Thanks for signing up. We're excited to have you!</p>
+        <p>You can now log in and start using your account.</p>
+        <br>
+        <p>Best regards,<br>JuiceFrog</p>
+    ";
+
+    $mail->AltBody = "Welcome! Thanks for signing up.";
+
+    $mail->send();
         // redirect to home page
         header('Location: /');
     } else {
