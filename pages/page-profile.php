74a826cce7273a096e422b8a6a8832391d80918c
Jules KJ <juljen2730@gmail.com>
2026-04-24 15:44:15 +0200

    simple profile layout and css

--- pages/page-profile.php
+++ pages/page-profile.php
@@ -5,10 +5,9 @@ try{
     require_once __DIR__ . "/../session_utils.php";
     require_once __DIR__ . "/../db.php";
        
-
-   $user =  "";
    
     if (!isset($_SESSION["user"])) {
+        $_SESSION['flash_state'] = "error";
         $_SESSION['flash_message'] = "Please login to see your profile";
         header('Location: /login');
         exit;
@@ -27,20 +26,40 @@ require_once __DIR__."/../micro-components/_header.php";
 
 ?>
 <main >
+    <section id="main-content">
+        <section class="user-info">
+        
+            <img src="https://placehold.co/600x400" alt="">
 
+            <section>
+                <h2>name</h2>
+                <p>adress zip</p>
+                <p>phone</p>
+
+            </section>
+
+
+
+        </section>
+        <section>
+
+            <h1>Profile</h1>
+            <h2>welcome back <?= _($user["user_forename"] . " ". $user["user_lastname"]) ?></h2>
+
+            
+            <?php
+
+                if(!empty($viewed_homes)){
+                    $items = $viewed_homes;
+                    $scroller_header = "Your recently viewed homes!";
+                    require_once __DIR__ . "/../micro-components/_scroller.php";
+                }
+            
+            ?>
+        </section>
+    </section>
+   
  
-    <h1>Profile</h1>
-    <h2>welcome back <?= _($user["user_forename"] . " ". $user["user_lastname"]) ?></h2>
-
-    <?php
-
-    if(!empty($viewed_homes)){
-        $items = $viewed_homes;
-        $scroller_header = "Your recently viewed homes!";
-        require_once __DIR__ . "/../micro-components/_scroller.php";
-    }
-    
-    ?>
 
     
 
